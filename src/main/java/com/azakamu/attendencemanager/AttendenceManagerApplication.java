afab91e620874d8c7f430dffba601bb50904284b
janlingen <jan-lingen@outlook.com>
2022-05-16 19:23:27 +0200

    Update gradle project name

---
+++ src/main/java/com/azakamu/attendencemanager/AttendenceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.azakamu.attendencemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendenceManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AttendenceManagerApplication.class, args);
+  }
+
+}
