f580ab806ce5019f3dfbb8ca935b344a1870b77f
janlingen <jan-lingen@outlook.com>
2022-05-19 17:57:20 +0200

    Add Exam classfields

--- src/main/java/com/azakamu/attendencemanager/domain/model/Exam.java
+++ src/main/java/com/azakamu/attendencemanager/domain/model/Exam.java
@@ -1,4 +1,16 @@
 package com.azakamu.attendencemanager.domain.model;
 
 public class Exam {
+
+  private final ExamId examId;
+  private final String name;
+  private final Timeframe timeframe;
+  private final Boolean online;
+
+  public Exam(ExamId examId, String name, Timeframe timeframe, Boolean online) {
+    this.examId = examId;
+    this.name = name;
+    this.timeframe = timeframe;
+    this.online = online;
+  }
 }
