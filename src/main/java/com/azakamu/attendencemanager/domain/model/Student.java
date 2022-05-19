0ea45ecd54a66b70dca5ded1d7a77f08d7480143
janlingen <jan-lingen@outlook.com>
2022-05-19 20:32:35 +0200

    Add Student classfields

--- src/main/java/com/azakamu/attendencemanager/domain/model/Student.java
+++ src/main/java/com/azakamu/attendencemanager/domain/model/Student.java
@@ -1,4 +1,21 @@
 package com.azakamu.attendencemanager.domain.model;
 
+import java.util.List;
+
 public class Student {
+
+  private final Long id;
+  private final String name;
+  private final String github_name;
+  private final List<Vacation> vacationList;
+  private final List<ExamId> examIdList;
+
+  public Student(Long id, String name, String github_name, List<Vacation> vacationList,
+                 List<ExamId> examIdList) {
+    this.id = id;
+    this.name = name;
+    this.github_name = github_name;
+    this.vacationList = vacationList;
+    this.examIdList = examIdList;
+  }
 }
