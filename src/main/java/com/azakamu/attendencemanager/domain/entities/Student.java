789880d3bcb02e94a6e58a974d89d87ceff9adef
janlingen <jan-lingen@outlook.com>
2022-05-25 16:33:28 +0200

    Add hashCode() and equals() to Student Entity

--- src/main/java/com/azakamu/attendencemanager/domain/entities/Student.java
+++ src/main/java/com/azakamu/attendencemanager/domain/entities/Student.java
@@ -5,6 +5,7 @@ import com.azakamu.attendencemanager.domain.values.Vacation;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 public class Student {
 
@@ -118,5 +119,22 @@ public class Student {
 
   public List<ExamId> getExamIdList() {
     return examIdList;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Student student = (Student) o;
+    return id.equals(student.id) && githubId.equals(student.githubId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, githubId);
   }
 }
