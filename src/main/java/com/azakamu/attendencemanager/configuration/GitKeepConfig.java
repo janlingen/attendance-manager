3e1eb4cf705f3ff46d1ba4fe94c23ffc06b0693c
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-19 20:28:39 +0200

    Add dummy config package and architecture testfile

---
+++ src/main/java/com/azakamu/attendencemanager/configuration/GitKeepConfig.java
@@ -0,0 +1,2 @@
+package com.azakamu.attendencemanager.configuration;public class GitKeepConfig {
+}
---
+++ src/test/java/com/azakamu/attendencemanager/architecture/ArchUnitTests.java
@@ -0,0 +1,2 @@
+package com.azakamu.attendencemanager.architecture;public class ArchUnitTests {
+}
