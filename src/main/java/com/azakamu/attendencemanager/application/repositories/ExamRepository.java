a0676a5b6b923a0af9e071a0470625310b6db5fd
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-19 20:32:03 +0200

    Update Repository interfaces

--- src/main/java/com/azakamu/attendencemanager/application/repositories/ExamRepository.java
+++ src/main/java/com/azakamu/attendencemanager/application/repositories/ExamRepository.java
@@ -1,5 +1,16 @@
 package com.azakamu.attendencemanager.application.repositories;
 
+import com.azakamu.attendencemanager.domain.entities.Exam;
+import com.azakamu.attendencemanager.domain.values.ExamId;
+import java.util.List;
+
 public interface ExamRepository {
 
+  void save(Exam exam);
+
+  Exam findById(ExamId examId);
+
+  List<Exam> findAllById(List<ExamId> examIds);
+
+  List<Exam> findAll();
 }
--- src/main/java/com/azakamu/attendencemanager/application/repositories/StudentRepository.java
+++ src/main/java/com/azakamu/attendencemanager/application/repositories/StudentRepository.java
@@ -1,5 +1,12 @@
 package com.azakamu.attendencemanager.application.repositories;
 
+import com.azakamu.attendencemanager.domain.entities.Student;
+
 public interface StudentRepository {
 
+  void save(Student student);
+
+  Student findById(Long id);
+
+  Student findByGithubId(String githubId);
 }
