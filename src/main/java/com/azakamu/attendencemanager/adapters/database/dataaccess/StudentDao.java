e0ae82280f2c614239c7258c3d1768843ca22bfb
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-21 20:37:31 +0200

    Add custom query to studentdao

--- src/main/java/com/azakamu/attendencemanager/adapters/database/dataaccess/StudentDao.java
+++ src/main/java/com/azakamu/attendencemanager/adapters/database/dataaccess/StudentDao.java
@@ -1,8 +1,14 @@
 package com.azakamu.attendencemanager.adapters.database.dataaccess;
 
 import com.azakamu.attendencemanager.adapters.database.datatransfer.entities.StudentDto;
+import java.util.Optional;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface StudentDao extends CrudRepository<StudentDto, Long> {
+
+  @Query("SELECT * FROM STUDENT S WHERE S.GITHUB_ID = :githubId")
+  Optional<StudentDto> findByGithubId(@Param("githubId") String githubId);
 
 }
