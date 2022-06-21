0fe4c10e1fea10935afc66acbac5592d2bb030ea
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-21 20:38:14 +0200

    Add full database implementation

--- src/main/java/com/azakamu/attendencemanager/adapters/database/implementation/ExamRepoImpl.java
+++ src/main/java/com/azakamu/attendencemanager/adapters/database/implementation/ExamRepoImpl.java
@@ -1,5 +1,54 @@
 package com.azakamu.attendencemanager.adapters.database.implementation;
 
-public class ExamRepoImpl {
+import com.azakamu.attendencemanager.adapters.database.dataaccess.ExamDao;
+import com.azakamu.attendencemanager.adapters.database.datatransfer.entities.ExamDto;
+import com.azakamu.attendencemanager.adapters.database.mapper.ExamMapper;
+import com.azakamu.attendencemanager.application.repositories.ExamRepository;
+import com.azakamu.attendencemanager.domain.entities.Exam;
+import com.azakamu.attendencemanager.domain.values.ExamId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class ExamRepoImpl implements ExamRepository {
+
+  private final ExamDao dataaccess;
+  private final ExamMapper mapper;
+
+  public ExamRepoImpl(ExamDao dataaccess, ExamMapper mapper) {
+    this.dataaccess = dataaccess;
+    this.mapper = mapper;
+  }
+
+  @Override
+  public void save(Exam exam) {
+    dataaccess.save(mapper.toDto(exam));
+  }
+
+  @Override
+  public Exam findById(ExamId examId) {
+    return dataaccess.findById(examId.id()).map(mapper::toDomain)
+        .orElse(Exam.createDummy());
+  }
+
+  @Override
+  public List<Exam> findAllById(List<ExamId> examIds) {
+    List<Exam> exams = new ArrayList<>();
+    for (ExamId examId : examIds) {
+      Exam exam = findById(examId);
+      if (!exam.getExamId().id().equals(-1L)) {
+        exams.add(exam);
+      }
+    }
+    return exams;
+  }
+
+  @Override
+  public List<Exam> findAll() {
+    List<ExamDto> exams = new ArrayList<>();
+    dataaccess.findAll().forEach(exams::add);
+    return exams.stream().map(mapper::toDomain).collect(Collectors.toList());
+  }
 }
--- src/main/java/com/azakamu/attendencemanager/adapters/database/implementation/StudentRepoImpl.java
+++ src/main/java/com/azakamu/attendencemanager/adapters/database/implementation/StudentRepoImpl.java
@@ -1,5 +1,34 @@
 package com.azakamu.attendencemanager.adapters.database.implementation;
 
-public class StudentRepoImpl {
+import com.azakamu.attendencemanager.adapters.database.dataaccess.StudentDao;
+import com.azakamu.attendencemanager.adapters.database.mapper.StudentMapper;
+import com.azakamu.attendencemanager.application.repositories.StudentRepository;
+import com.azakamu.attendencemanager.domain.entities.Student;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class StudentRepoImpl implements StudentRepository {
+
+  private final StudentDao dataaccess;
+  private final StudentMapper mapper;
+
+  public StudentRepoImpl(StudentDao dataaccess, StudentMapper mapper) {
+    this.dataaccess = dataaccess;
+    this.mapper = mapper;
+  }
+
+  @Override
+  public void save(Student student) {
+    dataaccess.save(mapper.toDto(student));
+  }
+
+  @Override
+  public Student findById(Long id) {
+    return dataaccess.findById(id).map(mapper::toDomain).orElse(Student.createDummy());
+  }
+
+  @Override
+  public Student findByGithubId(String githubId) {
+    return dataaccess.findByGithubId(githubId).map(mapper::toDomain).orElse(Student.createDummy());
+  }
 }
