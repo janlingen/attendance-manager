3c8312827535bbdd8f9a62c8d3e5d03c07762518
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-23 22:02:37 +0200

    Add mapper testfiles

---
+++ src/test/java/com/azakamu/attendencemanager/database/mapper/StudentMapperUnitTests.java
@@ -0,0 +1,51 @@
+package com.azakamu.attendencemanager.database.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.azakamu.attendencemanager.adapters.database.datatransfer.entities.StudentDto;
+import com.azakamu.attendencemanager.adapters.database.mapper.ExamIdMapper;
+import com.azakamu.attendencemanager.adapters.database.mapper.ExamMapper;
+import com.azakamu.attendencemanager.adapters.database.mapper.StudentMapper;
+import com.azakamu.attendencemanager.adapters.database.mapper.VacationMapper;
+import com.azakamu.attendencemanager.domain.entities.Student;
+import com.azakamu.attendencemanager.domain.values.ExamId;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+public class StudentMapperTest {
+
+  StudentMapper mapper = Mappers.getMapper(StudentMapper.class);
+  VacationMapper vacationMapper = Mappers.getMapper(VacationMapper.class);
+  ExamIdMapper examIdMapper = Mappers.getMapper(ExamIdMapper.class);
+
+  @Test
+  void DomainToDtoTest1() {
+    // arrange
+    Student student = Student.createDummy();
+
+    // act
+    StudentDto studentDto = mapper.toDto(student);
+
+    // assert
+    assertThat(studentDto.getId()).isEqualTo(student.getId());
+    assertThat(studentDto.getGithubName()).isEqualTo(student.getGithubName());
+    assertThat(studentDto.getGithubId()).isEqualTo(student.getGithubId());
+    assertThat(studentDto.getLeftoverVacationTime()).isEqualTo(student.getLeftoverVacationTime());
+    assertThat(vacationMapper.toDomainSet(studentDto.getVacations())).containsAll(
+        student.getVacations());
+    assertThat(examIdMapper.toDomainSet(studentDto.getExamIds())).containsAll(
+        student.getExamIds());
+  }
+
+  @Test
+  void DtoToDomainTest1() {
+    // arrange
+    StudentDto studentDto = mapper.toDto(Student.createDummy());
+
+    // act
+    Student student = mapper.toDomain(studentDto);
+
+    // assert
+    assertThat(student).isEqualTo(Student.createDummy());
+  }
+}
---
+++ src/test/java/com/azakamu/attendencemanager/database/mapper/TimeframeMapperUnitTests.java
@@ -0,0 +1,2 @@
+package com.azakamu.attendencemanager.database.mapper;public class TimeframeMapperUnitTests {
+}
