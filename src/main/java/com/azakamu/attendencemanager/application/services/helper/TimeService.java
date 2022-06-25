87bb805d438694b0784be0643326544fce541a42
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-25 16:48:05 +0200

    Update TimeService according to new needs

--- src/main/java/com/azakamu/attendencemanager/application/services/helper/TimeService.java
+++ src/main/java/com/azakamu/attendencemanager/application/services/helper/TimeService.java
@@ -11,15 +11,27 @@ public class TimeService {
   private final LocalDate timespanEnd;
   private final Integer exemptionOffsetOffline;
   private final Integer exemptionOffsetOnline;
+  private final Long vacationTime;
+  private final Integer intervallTime;
+  private final Integer timeBetween;
+  private final Integer maximumVacationLenght;
 
   public TimeService(@Value("${attendance-manager.timespanStart}") String timespanStart,
       @Value("${attendance-manager.timespanEnd}") String timespanEnd,
       @Value("${attendance-manager.exemptionOffsetOffline}") String exemptionOffsetOffline,
-      @Value("${attendance-manager.exemptionOffsetOnline}") String exemptionOffsetOnline) {
+      @Value("${attendance-manager.exemptionOffsetOnline}") String exemptionOffsetOnline,
+      @Value("${attendance-manager.vacationTime}") String vacationTime,
+      @Value("${attendance-manager.intervallTime}") String intervallTime,
+      @Value("${attendance-manager.timeBetween}") String timeBetween,
+      @Value("${attendance-manager.maximumVacationLenght}") String maximumVacationLenght) {
     this.timespanStart = LocalDate.parse(timespanStart);
     this.timespanEnd = LocalDate.parse(timespanEnd);
     this.exemptionOffsetOffline = Integer.parseInt(exemptionOffsetOffline);
     this.exemptionOffsetOnline = Integer.parseInt(exemptionOffsetOnline);
+    this.vacationTime = Long.parseLong(vacationTime);
+    this.intervallTime = Integer.parseInt(intervallTime);
+    this.timeBetween = Integer.parseInt(timeBetween);
+    this.maximumVacationLenght = Integer.parseInt(maximumVacationLenght);
   }
 
   public LocalDate getTimespanStart() {
@@ -39,5 +51,21 @@ public class TimeService {
 
   public Integer getExemptionOffsetOnline() {
     return exemptionOffsetOnline;
+  }
+
+  public Long getVacationTime() {
+    return vacationTime;
+  }
+
+  public Integer getIntervallTime() {
+    return intervallTime;
+  }
+
+  public Integer getMaximumVacationLenght() {
+    return maximumVacationLenght;
+  }
+
+  public Integer getTimeBetween() {
+    return timeBetween;
   }
 }
