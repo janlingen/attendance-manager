996a00eddb63f89e5045f31f3f31a30964da703f
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-26 16:43:47 +0200

    Add full security settings

--- src/main/java/com/azakamu/attendencemanager/configuration/MethodSecurityConfiguration.java
+++ src/main/java/com/azakamu/attendencemanager/configuration/MethodSecurityConfiguration.java
@@ -1,2 +1,11 @@
-package com.azakamu.attendencemanager.configuration;public class MethodSecurityConfiguration {
+package com.azakamu.attendencemanager.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+
 }
