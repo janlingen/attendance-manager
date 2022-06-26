cd3e19d4e93b225c2d752cb3dab1281a857b84f1
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-26 22:03:52 +0200

    Add all missing funcitonalities to StudentController

--- src/main/java/com/azakamu/attendencemanager/adapters/webcontroller/controller/StudentController.java
+++ src/main/java/com/azakamu/attendencemanager/adapters/webcontroller/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.azakamu.attendencemanager.adapters.webcontroller.controller;
 
+import com.azakamu.attendencemanager.adapters.webcontroller.datainput.ExamForm;
 import com.azakamu.attendencemanager.adapters.webcontroller.datainput.VacationForm;
 import com.azakamu.attendencemanager.application.services.ExamService;
 import com.azakamu.attendencemanager.application.services.StudentService;
 import com.azakamu.attendencemanager.application.services.helper.TimeService;
+import com.azakamu.attendencemanager.application.validators.ExamValidator;
 import com.azakamu.attendencemanager.application.validators.VacationValidator;
 import com.azakamu.attendencemanager.domain.entities.Exam;
 import com.azakamu.attendencemanager.domain.entities.Student;
@@ -73,9 +75,11 @@ public class StudentController {
   }
 
   @PostMapping("/cancelVacation")
-  public String cancelVacation(VacationForm form, @ModelAttribute("githubName") String githubName,
+  public String cancelVacation(VacationForm form,
+      @ModelAttribute("githubName") String githubName,
       @ModelAttribute("githubId") String githubId) {
-    studentService.cancelVacation(githubName, githubId, form.date(), form.start(), form.end(),
+    studentService.cancelVacation(githubName, githubId, form.date(), form.start(),
+        form.end(),
         form.reason());
     return "redirect:/";
   }
@@ -99,13 +103,13 @@ public class StudentController {
 
   @PostMapping("/enrollVacation")
   public String enrollVacation(
-      RedirectAttributes redirectAttributes,
-      @ModelAttribute("urlaub") VacationForm form,
+      RedirectAttributes redirectAttributes, VacationForm vacationForm,
       @ModelAttribute("githubName") String githubName,
       @ModelAttribute("githubId") String githubId) {
     VacationValidator result =
-        studentService.enrollVacation(githubName, githubId, form.date(), form.start(), form.end(),
-            form.reason());
+        studentService.enrollVacation(githubName, githubId, vacationForm.date(),
+            vacationForm.start(), vacationForm.end(),
+            vacationForm.reason());
     if (result == VacationValidator.SUCCESS) {
       return "redirect:/";
     } else {
@@ -113,5 +117,47 @@ public class StudentController {
       return "redirect:/vacationenrollment";
     }
   }
+
+  @GetMapping("/examenrollment")
+  public String examEnrollment(Model model) {
+    model.addAttribute("exams", examService.getUpcommingExams());
+    return "examenrollment";
+  }
+
+  @PostMapping("/enrollExam")
+  public String enrollExam(Long examid, @ModelAttribute("githubName") String githubName,
+      @ModelAttribute("githubId") String githubId) {
+    studentService.enrollExam(githubName, githubId, examid);
+    return "redirect:/";
+  }
+
+  @GetMapping("/examcreation")
+  public String examCreation(Model model) {
+    model.addAttribute("times", examTimes);
+    ExamValidator result = (ExamValidator) model.getAttribute("result");
+    if (result != null) {
+      model.addAttribute("errorMsg", result.getMsg());
+    }
+    return "examcreation";
+  }
+
+  @PostMapping("/createExam")
+  public String createExam(
+      RedirectAttributes redirectAttributes, @ModelAttribute("form") ExamForm form) {
+    ExamValidator result =
+        examService.createExam(
+            form.name(),
+            form.date(),
+            form.start(),
+            form.end(),
+            form.onlineExam());
+    if (result == ExamValidator.SUCCESS) {
+      return "redirect:/examenrollment";
+    } else {
+      redirectAttributes.addFlashAttribute("result", result);
+      return "redirect:/examcreation";
+    }
+  }
+
 
 }
