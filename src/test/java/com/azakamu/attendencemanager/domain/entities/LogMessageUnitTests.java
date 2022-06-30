0c25d74c545df2a4a2afed36afdb961af41bd841
janlingen <82512348+janlingen@users.noreply.github.com>
2022-06-30 20:15:55 +0200

    Add all unit test for Admin and Tutor

--- src/test/java/com/azakamu/attendencemanager/domain/entities/LogMessageUnitTests.java
+++ src/test/java/com/azakamu/attendencemanager/domain/entities/LogMessageUnitTests.java
@@ -1,2 +1,27 @@
-package com.azakamu.attendencemanager.domain.entities;public class LogMessageUnitTests {
+package com.azakamu.attendencemanager.domain.entities;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+
+public class LogMessageUnitTests {
+
+  @Test
+  void createDummyTest1() {
+    // arrange
+    LogMessage logMessage = new LogMessage("12345678", "light-saber training", -1L,
+        LocalDateTime.of(2022, 12, 24, 10, 10));
+
+    // act
+    LogMessage result = LogMessage.createDummy();
+
+    // assert
+    assertThat(result.getGithubId()).isEqualTo(logMessage.getGithubId());
+    assertThat(result.getCreatedAt()).isEqualTo(logMessage.getCreatedAt());
+    assertThat(result.getAction()).isEqualTo(logMessage.getAction());
+    assertThat(result.getId()).isEqualTo(logMessage.getId());
+
+
+  }
 }
